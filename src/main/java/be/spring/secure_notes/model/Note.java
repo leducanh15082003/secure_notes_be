20de0e774f83a5b3b2d2dce58afd263cd521d0e8
leducanh1508 <bokholeki@gmail.com>
2025-09-05 16:26:25 +0700

    fix json loop

--- src/main/java/be/spring/secure_notes/model/Note.java
+++ src/main/java/be/spring/secure_notes/model/Note.java
@@ -1,5 +1,6 @@
 package be.spring.secure_notes.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -23,5 +24,6 @@ public class Note {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
     private User owner;
 }
